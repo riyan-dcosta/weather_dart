bd7cfe182c5cbb3a47e72e3dde8ba05e06e844be
Riyan <rdriyan@gmail.com>
2023-12-13 23:02:40 +0530

    Added WeatherTheme class with ThemeData in MaterialApp.
     - currently added colorScheme from seed.
     - can add textTheme in future.
     - [design/themes](https://docs.flutter.dev/cookbook/design/themes)

---
+++ lib/core/config/themes/weather_theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class WeatherTheme {
+  static final colorScheme = ColorScheme.fromSeed(
+    seedColor: Colors.orange,
+    brightness: Brightness.dark,
+  );
+}
