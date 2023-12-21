1b7de2a0240a973e91f33d360d9dacc0b88c9dc7
Riyan <rdriyan@gmail.com>
2023-12-22 00:10:25 +0530

    Update counter_test.dart

--- test/counter_test.dart
+++ test/counter_test.dart
@@ -2,12 +2,16 @@ import 'package:test/test.dart';
 import 'package:weather_dart/counter/counter.dart';
 
 void main() {
+  late Counter counter;
+
+  setUp(() {
+    counter = Counter();
+  });
   group('counterIncrement', () {
-    test('Initial value must be zero', () {
-      expect(Counter().value, 0);
+P    test('Initial value must be zero', () {
+      expect(counter.value, 0);
     });
     test('Initial Counter after increment must be 1', () {
-      final counter = Counter();
       counter.increment();
       expect(counter.value, 1);
     });
